3088d22df4ad2d10543153d9cc805d48bcb6363c
Syz <76086565+Syz9@users.noreply.github.com>
2021-01-17 05:07:06 -0500

    e
    e

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title XYZ MODULES INSTALLER
+py -3.8 -m pip install -r requirements.txt
+echo Finished!
+pause
---
+++ launch.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title XYZ SELFBOT
+py -3.8 Selfbot.py
+echo RIP I don't want to make a loop for a user who doesn't want to understand :)
+pause
